5de6a34b4a755f02308b7fb8ce448de18a2ee3df
elyudwo <dudqk9696@naver.com>
2024-10-31 20:12:28 +0900

    Refactor: delete parentheses based on operator priority

--- deps/lua/src/lobject.c
+++ deps/lua/src/lobject.c
@@ -45,7 +45,7 @@ int luaO_int2fb (unsigned int x) {
 
 /* converts back */
 int luaO_fb2int (int x) {
-  int e = (x >> 3) & 31;
+  int e = x >> 3 & 31;
   if (e == 0) return x;
   else return ((x & 7)+8) << (e - 1);
 }
